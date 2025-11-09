db406fc56c18b7617b69c8c9fb5398cec5a534de
Nadezhda Memelova <nadezhdanik@gmail.com>
2025-11-09 13:30:15 +0200

    feat: add nodemon and cross-env, add dev script

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { PORT } from "./config";
-import { createAppServer } from "./server";
+import { PORT } from "./config.ts";
+import { createAppServer } from "./server.ts";
 
 createAppServer(PORT);
--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,10 @@ export const createAppServer = (port: number) => {
   const server = createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Server is running" }));
+  });
 
-    server.listen(port, () => {
-      console.log(`Server running at http://localhost:${port}`);
-    });
+  server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
   });
 
   return server;
