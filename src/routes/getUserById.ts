796c2e71fef6a5c921ad5526ff814d3bf4615837
Nadezhda Memelova <nadezhdanik@gmail.com>
2025-11-09 22:35:26 +0200

    fix: fix scripts for dev and prod modes

--- src/routes/getUserById.ts
+++ src/routes/getUserById.ts
@@ -1,5 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
-import { users } from "../store/userStore.js";
+import { users } from "../store/userStore.ts";
 import { sendJson } from "../utils/sendJson.ts";
 import { StatusCodes } from "../constants/statusCodes.ts";
 import { Methods } from "../constants/methods.ts";
